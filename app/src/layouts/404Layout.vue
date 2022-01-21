722e16244299d97fbad262cbed50edf8253b2f11
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-21 22:36:34 +0100

    style: (wip) implement 404Layout's buttons layout

--- app/src/layouts/404Layout.vue
+++ app/src/layouts/404Layout.vue
@@ -7,13 +7,17 @@
   <main class="px-4 py-10 text-center text-teal-700 dark:text-gray-200">
     <carbon-warning class="inline-block text-4xl" />
     <router-view />
-    <div w:m="3 t-8" w:text="sm" w:flex="~" w:gap="x-4" w:justify="center">
-      <button class="btn">
-        {{ t('Home') }}
-      </button>
-      <button class="btn" @click="router.back()">
-        {{ t('Back') }}
-      </button>
+    <div w:display="inline" w:m="3 t-8" w:children="table-cell w-1/2 p-4 children:w-full">
+      <span>
+        <button class="btn">
+          <mdi-home />
+        </button>
+      </span>
+      <span>
+        <button class="btn" @click="router.back()">
+          <mdi-arrow-left-thick />
+        </button>
+      </span>
     </div>
   </main>
 </template>
