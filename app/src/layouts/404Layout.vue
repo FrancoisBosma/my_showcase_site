eb021508e8d452dda8bff8055b8280b1a2005051
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-21 21:16:24 +0100

    style: add Home button in 404Layout

--- app/src/layouts/404Layout.vue
+++ app/src/layouts/404Layout.vue
@@ -5,14 +5,13 @@
 
 <template>
   <main class="px-4 py-10 text-center text-teal-700 dark:text-gray-200">
-    <div>
-      <p class="text-4xl">
-        <carbon-warning class="inline-block" />
-      </p>
-    </div>
+    <carbon-warning class="inline-block text-4xl" />
     <router-view />
-    <div>
-      <button class="btn m-3 text-sm mt-8" @click="router.back()">
+    <div w:m="3 t-8" w:text="sm" w:flex="~" w:gap="x-4" w:justify="center">
+      <button class="btn">
+        {{ t('Home') }}
+      </button>
+      <button class="btn" @click="router.back()">
         {{ t('Back') }}
       </button>
     </div>
