ff364b74d3da2af80de97bdf5993030895ed50b3
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-21 21:10:25 +0100

    chore: replace project header and description

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,8 +3,8 @@
   // you can use this to manipulate the document head in any components,
   // they will be rendered correctly in the html results with vite-ssg
   useHead({
-    title: 'Rapide-et-furieux',
-    meta: [{ name: 'description', content: 'Opinionated Vite Starter Template' }],
+    title: 'Francois Bosma | Vue.js developer | Freelancer',
+    meta: [{ name: 'description', content: 'Showcase site for Vue.js freelance developer Francois BOSMA' }],
   })
 </script>
 
