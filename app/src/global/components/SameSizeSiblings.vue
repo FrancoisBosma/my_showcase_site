3016a891e1b27775e370d5c5b3f4d79d9b96eb3e
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-22 15:22:27 +0100

    feat: implement new component SameSizeSiblings

---
+++ app/src/global/components/SameSizeSiblings.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const slots = useSlots()
+  const slotNb = Object.keys(slots).length
+</script>
+
+<template>
+  <div w:display="inline" w:m="3 t-8" w:children="table-cell w-1/2 p-4 children:(flex w-full)">
+    <span v-for="index in slotNb" :key="index">
+      <slot :name="index"></slot>
+    </span>
+  </div>
+</template>
