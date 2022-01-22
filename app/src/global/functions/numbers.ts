6b9fe9fdbaf921461bf9078dfb62eff8b7229686
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-22 20:50:29 +0100

    fix: update the numbers 'range' fn

--- app/src/global/functions/numbers.ts
+++ app/src/global/functions/numbers.ts
@@ -1,2 +1,2 @@
 export const range = (end: number, start = 0, step = 1) =>
-  Array.from({ length: Math.ceil((end - start) / step) }, (_, i) => start + i * step)
+  Array.from({ length: Math.ceil((end + 1 - start) / step) }, (_, i) => start + i * step)
