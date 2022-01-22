1da216eca05d2a1dc81185e1cb15145ecc5bafc2
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-22 13:18:24 +0100

    style: implement 404Layout's buttons layout

--- app/src/layouts/404Layout.vue
+++ app/src/layouts/404Layout.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
   const router = useRouter()
-  const { t } = useI18n()
 </script>
 
 <template>
   <main class="px-4 py-10 text-center text-teal-700 dark:text-gray-200">
     <carbon-warning class="inline-block text-4xl" />
     <router-view />
-    <div w:display="inline" w:m="3 t-8" w:children="table-cell w-1/2 p-4 children:w-full">
+    <div w:display="inline" w:m="3 t-8" w:children="table-cell w-1/2 p-4 children:(flex w-full)">
       <span>
-        <button class="btn">
-          <mdi-home />
+        <button class="btn" @click="router.push('/')">
+          <mdi-home style="font-size: 25px" />
         </button>
       </span>
       <span>
         <button class="btn" @click="router.back()">
-          <mdi-arrow-left-thick />
+          <mdi-arrow-left-thick style="font-size: 25px" />
         </button>
       </span>
     </div>
