3c316cb817c98b295e52b05dcf4018456da913e2
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-22 19:02:31 +0100

    feat: implement new component BurgerButton

---
+++ app/src/global/components/BurgerButton.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+  withDefaults(defineProps<{ action?: Function }>(), { action: () => () => undefined })
+</script>
+
+<template>
+  <div @click="action()">BB</div>
+</template>
