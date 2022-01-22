c4197a3754cf08685ee07a859833c7101715429d
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-22 19:04:05 +0100

    feat: add side menu to LandingPage

--- app/src/features/LandingPage/core/router-views/index.vue
+++ app/src/features/LandingPage/core/router-views/index.vue
@@ -6,11 +6,16 @@ meta:
 
 <script setup lang="ts">
   import { Contact, Introduction, Playground, Skills } from '../components'
+
+  const isMenuVisible = ref(false)
 </script>
 
 <template>
-  <Introduction />
-  <Skills />
-  <Playground />
-  <Contact />
+  <menu class="fixed left-0 h-screen m-0 w-52" w:border="1 solid red-800">{{ isMenuVisible }}</menu>
+  <div class="pl-52">
+    <Introduction />
+    <Skills />
+    <Playground />
+    <Contact />
+  </div>
 </template>
