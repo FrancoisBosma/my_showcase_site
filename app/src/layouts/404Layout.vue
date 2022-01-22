f6cdcd200d283578f9c3c0579ecd6974e68eea52
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-22 15:23:06 +0100

    refactor: reimplement 404Layout's buttons using SameSizeSiblings component

--- app/src/layouts/404Layout.vue
+++ app/src/layouts/404Layout.vue
@@ -6,17 +6,17 @@
   <main class="px-4 py-10 text-center text-teal-700 dark:text-gray-200">
     <carbon-warning class="inline-block text-4xl" />
     <router-view />
-    <div w:display="inline" w:m="3 t-8" w:children="table-cell w-1/2 p-4 children:(flex w-full)">
-      <span>
+    <SameSizeSiblings>
+      <template #1>
         <button class="btn" @click="router.push('/')">
           <mdi-home style="font-size: 25px" />
         </button>
-      </span>
-      <span>
+      </template>
+      <template #2>
         <button class="btn" @click="router.back()">
           <mdi-arrow-left-thick style="font-size: 25px" />
         </button>
-      </span>
-    </div>
+      </template>
+    </SameSizeSiblings>
   </main>
 </template>
