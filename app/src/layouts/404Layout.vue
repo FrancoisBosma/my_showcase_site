3f3d799fa138e8c63203f7fdf45b836e4632e28b
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-23 18:31:55 +0100

    style: css cleanup

--- app/src/layouts/404Layout.vue
+++ app/src/layouts/404Layout.vue
@@ -3,18 +3,18 @@
 </script>
 
 <template>
-  <main class="px-4 py-10 text-center text-teal-700 dark:text-gray-200">
+  <main class="px-4 py-10">
     <carbon-warning class="inline-block text-4xl" />
     <router-view />
     <SameSizeSiblings>
       <template #1>
         <button class="btn" @click="router.push('/')">
-          <mdi-home style="font-size: 25px" />
+          <mdi-home w:text="2xl" />
         </button>
       </template>
       <template #2>
         <button class="btn" @click="router.back()">
-          <mdi-arrow-left-thick style="font-size: 25px" />
+          <mdi-arrow-left-thick w:text="2xl" />
         </button>
       </template>
     </SameSizeSiblings>
