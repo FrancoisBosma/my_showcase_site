9634411d4eab2208dee407bc0b0a4e7de3fc7336
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-23 20:55:02 +0100

    feat: fill in introduction's text

--- app/src/features/LandingPage/core/components/Introduction/Introduction.vue
+++ app/src/features/LandingPage/core/components/Introduction/Introduction.vue
@@ -1,3 +1,11 @@
 <template>
-  <section id="introduction" w:h="screen" w:border="1 solid light-50">Introduction</section>
+  <section
+    id="introduction"
+    w:flex="~ col"
+    class="p-12 h-screen items-start justify-center"
+    w:border="1 solid light-50"
+  >
+    <h1 class="text-left">Hi,<br />I'm Francois,<br />web developer</h1>
+    <p class="py-4">Vue.js enthusiast | full-stack dev | Freelancer</p>
+  </section>
 </template>
