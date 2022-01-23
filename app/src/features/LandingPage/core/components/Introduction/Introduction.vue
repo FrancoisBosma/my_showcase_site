b8fa7197cb3f3430cc2676a1195a2c6c573a00af
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-23 21:20:52 +0100

    refactor: use i18n instead of plain text

--- app/src/features/LandingPage/core/components/Introduction/Introduction.vue
+++ app/src/features/LandingPage/core/components/Introduction/Introduction.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+  const { t } = useI18n()
+</script>
 <template>
   <section
     id="introduction"
@@ -5,7 +8,7 @@
     class="p-12 h-screen items-start justify-center text-left"
     w:border="1 solid light-50"
   >
-    <h1>Hi,<br />I'm Francois,<br />web developer</h1>
+    <h1>{{ t('Hi,') }}<br />{{ t("I'm Francois,") }}<br />{{ t('web developer') }}</h1>
     <p class="py-4">Vue.js enthusiast | full-stack dev | Freelancer</p>
   </section>
 </template>
