1ee49f63f36ef05e0d186f99b78bff9de90ac274
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-23 21:06:33 +0100

    style: introduction: hoist 'text-left' to the section

--- app/src/features/LandingPage/core/components/Introduction/Introduction.vue
+++ app/src/features/LandingPage/core/components/Introduction/Introduction.vue
@@ -2,10 +2,10 @@
   <section
     id="introduction"
     w:flex="~ col"
-    class="p-12 h-screen items-start justify-center"
+    class="p-12 h-screen items-start justify-center text-left"
     w:border="1 solid light-50"
   >
-    <h1 class="text-left">Hi,<br />I'm Francois,<br />web developer</h1>
+    <h1>Hi,<br />I'm Francois,<br />web developer</h1>
     <p class="py-4">Vue.js enthusiast | full-stack dev | Freelancer</p>
   </section>
 </template>
