a462c01ecb3472fb32e449ca83a5514f3b827b9d
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-23 21:28:18 +0100

    feat: implement skills section's layout

--- app/src/features/LandingPage/core/components/Skills/Skills.vue
+++ app/src/features/LandingPage/core/components/Skills/Skills.vue
@@ -1,3 +1,15 @@
 <template>
-  <section id="skills" w:border="1 solid light-50">Skills</section>
+  <section id="skills" class="p-12" w:border="1 solid light-50">
+    <h2>Skills</h2>
+    <ul class="p-4 grid grid-cols-4 grid-rows-2">
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+    </ul>
+  </section>
 </template>
