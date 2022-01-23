b9443bd5104b13c3452803bc1eaf45fdae582b0e
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-23 22:03:58 +0100

    feat: add icons to 'skills' section

--- app/src/features/LandingPage/core/components/Skills/Skills.vue
+++ app/src/features/LandingPage/core/components/Skills/Skills.vue
@@ -1,11 +1,11 @@
 <template>
   <section id="skills" class="p-12" w:border="1 solid light-50">
     <h2>Skills</h2>
-    <ul class="p-4 grid grid-cols-4 grid-rows-2">
-      <li>A</li>
-      <li>A</li>
-      <li>A</li>
-      <li>A</li>
+    <ul class="p-4 grid grid-cols-4 grid-rows-2 text-50px">
+      <li><mdi-diamond-stone /></li>
+      <li><ant-design-format-painter-outlined /></li>
+      <li><clarity-ruler-pencil-line /></li>
+      <li><gg-heart /></li>
       <li>A</li>
       <li>A</li>
       <li>A</li>
