5030530fd29a1e3593f5ff709ed132e33b35ae42
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-24 20:07:32 +0100

    chore: add i18n to 'Playground' section

--- app/src/features/LandingPage/core/components/Playground/Playground.vue
+++ app/src/features/LandingPage/core/components/Playground/Playground.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+  const { t } = useI18n()
+</script>
 <template>
-  <section id="playground" w:border="1 solid light-50">Playground</section>
+  <section id="playground" class="min-h-screen" w:border="1 solid light-50">
+    <h2>{{ t('Playground') }}</h2>
+  </section>
 </template>
