30c7f633b7ae5e018b9d9953e45cb0efb078a073
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-24 16:52:39 +0100

    chore: add windicss' plugin 'aspect-ratio' to project

--- app/windi.config.ts
+++ app/windi.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     prefix: 'w:',
   },
   safelist: [range(20, 2).map((i) => `w-1/${i}`)],
-  plugins: [],
+  plugins: [require('windicss/plugin/aspect-ratio')],
   theme: {
     extend: {
       animation: {
