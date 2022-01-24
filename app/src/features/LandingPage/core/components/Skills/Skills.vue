4831f11890d3931f6f863041c8dc174f33141b3d
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-24 18:05:24 +0100

    style: update the checkerboard layout and content

--- app/src/features/LandingPage/core/components/Skills/Skills.vue
+++ app/src/features/LandingPage/core/components/Skills/Skills.vue
@@ -3,14 +3,14 @@
     <h2>Skills</h2>
     <!-- <ul class="grid grid-cols-2 grid-rows-4 text-50px
     children:(border-1 border-solid border-light-50 aspect-square)"> -->
-    <ul class="grid grid-flow-col grid-cols-4 grid-rows-2 text-50px toto">
-      <li><mdi-diamond-stone /></li>
-      <li>A</li>
-      <li>A</li>
+    <ul class="grid grid-flow-col grid-cols-4 grid-rows-2 toto">
+      <li><fa-diamond /></li>
+      <li>World-Class Apps</li>
+      <li>Tailored To Your Needs</li>
       <li><ant-design-format-painter-outlined /></li>
       <li><clarity-ruler-pencil-line /></li>
-      <li>A</li>
-      <li>A</li>
+      <li>Pixel-Perfect Code</li>
+      <li>Made With Passion</li>
       <li><gg-heart /></li>
     </ul>
   </section>
@@ -18,14 +18,11 @@
 <style scoped lang="postcss">
   .toto {
     li {
-      @apply flex justify-center items-center;
-    }
-    & > * {
-      @apply bg-[var(--info)] aspect-square;
+      @apply flex justify-center items-center aspect-square bg-[var(--background)] text-[var(--foreground)] text-4xl;
     }
     li:nth-child(4n),
     li:nth-child(4n + 1) {
-      @apply bg-[var(--background)];
+      @apply bg-[var(--info)] text-[var(--background)] text-6xl;
     }
   }
 </style>
