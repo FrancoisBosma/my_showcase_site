531f445cdeb8d71a5a2f5eab353da2b25f0d6f60
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-27 20:29:36 +0100

    style: (wip) LanguageSelector: make menu toggle change the context color

--- app/src/global/components/LanguageSelector.vue
+++ app/src/global/components/LanguageSelector.vue
@@ -1,34 +1,53 @@
+<script setup lang="ts">
+  const bActive = ref(false)
+  const fnToggleSelector = () => (bActive.value = !bActive.value)
+  const contextColor = computed(() => (bActive.value ? 'var(--emphasis)' : 'var(--cta)'))
+  // const toggleSpeed = computed(() => (bActive.value ? '0ms' : '350ms'))
+</script>
+
 <template>
-  <button class="language-selector-btn">
+  <div>
+    <Popper placement="auto-start" arrow @open:popper="fnToggleSelector()" @close:popper="fnToggleSelector()">
+      <button class="language-selector-btn" :is-active="bActive">
+        <clarity-language-solid />
+      </button>
+      <template #content>
+        <div> Hello there </div>
+      </template>
+    </Popper>
+  </div>
+  <!-- <button class="language-selector-btn">
     <clarity-language-solid />
-  </button>
+  </button> -->
 </template>
 <style scoped lang="postcss">
   .language-selector-btn {
     @apply relative flex justify-center items-center w-13 h-13
       bg-[var(--foreground)] text-[var(--background)] rounded-[20%]
       duration-350 ease-in;
+    /* FIXME: DURATION TO BE FLEXIBLE HEREEEEEEEEEEE VVV */
     & > svg {
-      @apply relative top-0.5 duration-700 ease-out;
+      @apply relative top-0.5 transition duration-700 ease-out;
     }
     &::before {
       @apply absolute top-0 left-0 w-full h-full z-behind rounded-[20%]
-        bg-[var(--cta)]
         transform scale-90 duration-350 ease-in;
       content: '';
+      background-color: v-bind('contextColor');
     }
-
-    &:hover {
-      @apply bg-[var(--background)] text-[var(--cta)];
-      box-shadow: 0 0 5px var(--cta);
-      text-shadow: 0 0 5px var(--cta);
-      & > svg {
-        @apply transform rotate-y-360 transition duration-700 ease-out;
-      }
+  }
+  .language-selector-btn:hover,
+  [is-active='true'] {
+    @apply bg-[var(--background)];
+    color: v-bind('contextColor');
+    box-shadow: 0 0 5px v-bind('contextColor');
+    text-shadow: 0 0 5px v-bind('contextColor');
+    & > svg {
+      @apply transform rotate-y-360;
     }
-    &:hover::before {
-      @apply transform scale-110 duration-700 ease-out;
-      box-shadow: 0 0 15px var(--cta);
+    &::before {
+      @apply transform scale-110 ease-out;
+      box-shadow: 0 0 15px v-bind('contextColor');
     }
   }
 </style>
