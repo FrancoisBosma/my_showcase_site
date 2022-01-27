41d2ad6f27d7c37a2887bdcb90ebca35397afd8f
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-27 19:14:42 +0100

    style: add global style to Popper global component

--- app/src/global/components/Popper.vue
+++ app/src/global/components/Popper.vue
@@ -10,3 +10,16 @@
     </template>
   </Popper>
 </template>
+<style lang="postcss">
+  :root {
+    --popper-theme-background-color: var(--info);
+    --popper-theme-background-color-hover: var(--emphasis);
+    --popper-theme-text-color: var(--foreground);
+    --popper-theme-border-width: 0px;
+    --popper-theme-border-style: solid;
+    --popper-theme-border-color: #eeeeee;
+    --popper-theme-border-radius: 6px;
+    --popper-theme-padding: 32px;
+    --popper-theme-box-shadow: 0 6px 30px -6px rgba(0, 0, 0, 0.25);
+  }
+</style>
