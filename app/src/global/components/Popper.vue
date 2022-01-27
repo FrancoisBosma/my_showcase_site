f28ea2f56216d1bdfedad9d6908f34991159306c
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-27 18:00:53 +0100

    feat: implement 'Popper' global component

---
+++ app/src/global/components/Popper.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import Popper from 'vue3-popper'
+</script>
+
+<template>
+  <Popper placement="top" arrow>
+    <slot name="default" />
+    <template #content>
+      <slot name="content" />
+    </template>
+  </Popper>
+</template>
