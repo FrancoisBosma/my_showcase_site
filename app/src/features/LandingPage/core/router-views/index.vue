b78bf7d61a011a290e6ec607cb3b7539eb1ea6b3
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-27 13:41:23 +0100

    style: hide horizontal overflow on sections

--- app/src/features/LandingPage/core/router-views/index.vue
+++ app/src/features/LandingPage/core/router-views/index.vue
@@ -10,7 +10,7 @@ meta:
 
 <template>
   <Menu />
-  <div class="xl:pl-52 children:(relative p-12 min-h-screen flex flex-col justify-center)">
+  <div class="xl:pl-52 children:(relative p-12 min-h-screen overflow-x-hidden flex flex-col justify-center)">
     <Introduction />
     <Playground />
     <Skills />
