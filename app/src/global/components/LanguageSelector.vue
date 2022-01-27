94d9250752a9156b6011fc9dc976dca402b3c0bc
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-27 17:20:31 +0100

    style: rearrange languageselector's css code

--- app/src/global/components/LanguageSelector.vue
+++ app/src/global/components/LanguageSelector.vue
@@ -8,31 +8,28 @@
     @apply relative flex justify-center items-center w-13 h-13
       bg-[var(--foreground)] text-[var(--background)] rounded-[20%]
       duration-350 ease-in;
+    &:hover {
+      @apply bg-[var(--background)] text-[var(--cta)];
+      box-shadow: 0 0 5px var(--cta);
+      text-shadow: 0 0 5px var(--cta);
+    }
 
     & > svg {
       @apply relative top-0.5 duration-700 ease-out;
+      &:hover {
+        @apply transform rotate-y-360 transition duration-700 ease-out;
+      }
     }
-  }
 
-  .language-selector-btn:hover {
-    @apply bg-[var(--background)] text-[var(--cta)];
-    box-shadow: 0 0 5px var(--cta);
-    text-shadow: 0 0 5px var(--cta);
-
-    & > svg {
-      @apply transform rotate-y-360 transition duration-700 ease-out;
+    &::before {
+      @apply absolute top-0 left-0 w-full h-full z-behind rounded-[20%]
+        bg-[var(--cta)]
+        transform scale-90 duration-350 ease-in;
+      content: '';
     }
-  }
-
-  .language-selector-btn::before {
-    @apply absolute top-0 left-0 w-full h-full z-behind rounded-[20%]
-      bg-[var(--cta)]
-      transform scale-90 duration-350 ease-in;
-    content: '';
-  }
-
-  .language-selector-btn:hover::before {
-    @apply transform scale-110 duration-700 ease-out;
-    box-shadow: 0 0 15px var(--cta);
+    &:hover::before {
+      @apply transform scale-110 duration-700 ease-out;
+      box-shadow: 0 0 15px var(--cta);
+    }
   }
 </style>
