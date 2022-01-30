ba872442232c3dbe5da81e03dbdd148d3a307141
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-30 18:48:13 +0100

    style: remove confusing empty lines

--- app/src/global/components/DarkModeSwitcher.vue
+++ app/src/global/components/DarkModeSwitcher.vue
@@ -35,15 +35,12 @@
     &[is-dark='true'] ~ .toggle-switch {
       @apply ml-0 border-[#dee1c5] bg-[#fffdf2];
     }
-
     &[is-dark='true'] ~ .toggle-bg {
       @apply bg-[var(--foreground-dark)] border-[#202020];
     }
-
     &[is-dark='true'] ~ .toggle-switch .toggle-switch-figure {
       @apply ml-40px opacity-0 transform scale-10;
     }
-
     &[is-dark='true'] ~ .toggle-switch .toggle-switch-figureAlt {
       @apply transform scale-75;
     }
