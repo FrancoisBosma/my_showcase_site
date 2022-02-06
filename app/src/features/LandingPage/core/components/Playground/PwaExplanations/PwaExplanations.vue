949e12433791457a607ec6c5583b611417f90d79
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-06 20:00:45 +0100

    feat: add some style to PwaExplanations

--- app/src/features/LandingPage/core/components/Playground/PwaExplanations/PwaExplanations.vue
+++ app/src/features/LandingPage/core/components/Playground/PwaExplanations/PwaExplanations.vue
@@ -1,31 +1,30 @@
 <script setup lang="ts">
   import Flicking from '@egjs/vue3-flicking'
-  import { Arrow, Fade, Pagination, Parallax } from '@egjs/flicking-plugins'
+  import { Arrow, Fade, Pagination } from '@egjs/flicking-plugins'
 
   const flickingOptions = {
-    align: { camera: '25%', panel: '25%' },
-    circular: false,
+    align: 'prev',
     deceleration: 0.01,
     easing: (x: number) => 1 - (1 - x) ** 3,
-    noPanelStyleOverride: true,
-    // panelsPerView: 2,
   }
-  const flickingPlugins = [new Arrow({ parentEl: document.body }), new Fade(), new Pagination(), new Parallax()]
+  const flickingPlugins = [new Arrow({ parentEl: document.body }), new Fade(), new Pagination()]
   const { t } = useI18n()
 </script>
 <template>
-  <Flicking :options="flickingOptions" :plugins="flickingPlugins">
-    <div class="card-panel">1</div>
-    <div class="card-panel">2</div>
-    <div class="card-panel">3</div>
-    <div class="card-panel">4</div>
-    <div class="card-panel">5</div>
-    <template #viewport>
-      <span class="flicking-arrow-prev is-outside"></span>
-      <span class="flicking-arrow-next is-outside"></span>
-      <div class="flicking-pagination"></div>
-    </template>
-  </Flicking>
+  <div id="pwa-explanations">
+    <Flicking :options="flickingOptions" :plugins="flickingPlugins">
+      <div class="card-panel">1</div>
+      <div class="card-panel">2</div>
+      <div class="card-panel">3</div>
+      <div class="card-panel">4</div>
+      <div class="card-panel">5</div>
+      <template #viewport>
+        <div class="flicking-pagination"></div>
+      </template>
+    </Flicking>
+    <span class="flicking-arrow-prev is-outside"></span>
+    <span class="flicking-arrow-next is-outside"></span>
+  </div>
 </template>
 <style lang="postcss">
   @import '@egjs/vue3-flicking/dist/flicking.css';
@@ -34,21 +33,64 @@
   /* Imported from 'flicking' component's css (cf. index.html link tag)
    * N.B: this class has to be declared before, hence globally here
   */
+  .flicking-pagination-bullet {
+    @apply bg-[var(--cta)];
+  }
   .flicking-pagination-bullet-active {
-    background-color: var(--cta);
+    @apply bg-[var(--emphasis)];
+  }
+  /* Custom */
+  #pwa-explanations {
+    @apply relative left-16;
+    width: min(66rem, calc(100% - 5rem));
   }
 </style>
 <style scoped lang="postcss">
   /* Imported from 'flicking' component's css */
   .flicking-viewport {
-    /* overflow-x: clip;
-    overflow-y: visible; */
+    @apply overflow-hidden pb-12;
   }
   .flicking-pagination {
-    @apply -bottom-12;
+    @apply -bottom-4;
+  }
+  .flicking-arrow-prev {
+    &:before {
+      @apply transform rotate-315 duration-500 ease-out;
+    }
+    &:after {
+      @apply transform -rotate-315 duration-500 ease-out;
+    }
+  }
+  .flicking-arrow-next {
+    &:before {
+      @apply transform -rotate-315 duration-500 ease-out;
+    }
+    &:after {
+      @apply transform rotate-315 duration-500 ease-out;
+    }
+  }
+  .flicking-arrow-prev,
+  .flicking-arrow-next {
+    @apply transform -translate-y-[90%];
+    &.flicking-arrow-disabled {
+      &:before,
+      &:after {
+        @apply bg-[var(--background-stronger)] opacity-0 transform rotate-0 duration-500 ease-in;
+      }
+    }
+    &:not(.flicking-arrow-disabled) {
+      &:before,
+      &:after {
+        @apply bg-[var(--cta)];
+      }
+      &:hover:before,
+      &:hover:after {
+        @apply bg-[var(--emphasis)];
+      }
+    }
   }
   /* Custom */
   .card-panel {
-    @apply bg-[var(--foreground-contrast)] h-48;
+    @apply bg-[var(--foreground-contrast)] w-96 h-48;
   }
 </style>
