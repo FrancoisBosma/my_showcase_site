53402a803bd0e98d057c0265f6406a8433bd9bb7
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-08 21:31:58 +0100

    refactor: enhance layout within Playground

--- app/src/features/LandingPage/core/components/Playground/Playground.vue
+++ app/src/features/LandingPage/core/components/Playground/Playground.vue
@@ -76,7 +76,7 @@
         @apply text-right border-r border-[var(--foreground)] hover-emphasis;
       }
       & > div:nth-child(2) {
-        @apply px-16 flex flex-col gap-4 justify-center items-center overflow-x-hidden;
+        @apply px-16 flex flex-col gap-8 justify-center items-center overflow-x-hidden;
       }
       &.incoming {
         @apply discreet italic;
