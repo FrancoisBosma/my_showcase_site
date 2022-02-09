e199c8049024b1dd1e408706e89c48c919e79435
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-09 15:03:58 +0100

    chore: remove unneeded commented code

--- app/src/layouts/404Layout.vue
+++ app/src/layouts/404Layout.vue
@@ -7,12 +7,6 @@
   <main class="px-4 py-10">
     <carbon-warning class="inline-block text-4xl" />
     <router-view />
-    <!-- <SameSizeSiblings>
-      <template #1>
-      </template>
-      <template #2>
-      </template>
-    </SameSizeSiblings> -->
     <Button @click="router.push('/')"> {{ t('Home') }} </Button>
     <Button @click="router.push('/')"> {{ t('Back') }} </Button>
   </main>
