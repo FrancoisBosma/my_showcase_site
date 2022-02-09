778eb28d32083a05f984373a36e716016f7da42c
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-09 15:41:50 +0100

    fix: make IconButton more easily usable within tags with backgrounds

--- app/src/global/components/IconButton.vue
+++ app/src/global/components/IconButton.vue
@@ -9,13 +9,16 @@
 </template>
 <style scoped lang="postcss">
   .icon-button {
-    @apply relative flex justify-center items-center w-13 h-13
-      bg-[var(--foreground-contrast)] text-[var(--foreground)] rounded-1/2
-      duration-250 ease-out;
+    @apply relative flex justify-center items-center w-13 h-13 text-[var(--foreground)] rounded-1/2
+      duration-250 ease-out z-1;
     & > :slotted(svg) {
       @apply relative top-0.5 text-3xl transition-transform duration-250 ease-out;
     }
     &::before {
+      @apply absolute top-0 left-0 w-full h-full rounded-1/2 bg-[var(--foreground-contrast)];
+      content: '';
+    }
+    &::after {
       @apply absolute top-0 left-0 w-full h-full z-behind rounded-1/2
         transform scale-90 duration-500 ease-out bg-[var(--emphasis)];
       content: '';
@@ -24,7 +27,7 @@
     &:hover,
     &[is-active='true'] {
       @apply text-[var(--emphasis)];
-      &::before {
+      &::after {
         @apply transform scale-110 ease-out;
         box-shadow: 0 0 15px var(--emphasis);
       }
