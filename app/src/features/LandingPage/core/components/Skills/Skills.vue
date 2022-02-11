e77526a7eda32ca045b49867088f5c381dc4062b
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-11 23:37:34 +0100

    refactor: (wip) rework the style of the Skills section

--- app/src/features/LandingPage/core/components/Skills/Skills.vue
+++ app/src/features/LandingPage/core/components/Skills/Skills.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
   const { t } = useI18n()
 </script>
 <template>
@@ -27,6 +27,60 @@
     li:nth-child(4n),
     li:nth-child(4n + 1) {
       @apply bg-[var(--info)] text-[var(--background)] text-6xl;
+    }
+  }
+</style> -->
+<script setup lang="ts">
+  const { t } = useI18n()
+</script>
+<template>
+  <section id="skills" class="skills">
+    <h3>{{ t('Skills') }}</h3>
+    <ul>
+      <ul>
+        <li>
+          <figure>
+            <fa-diamond />
+            <figcaption>{{ t('World-Class Apps') }}</figcaption>
+          </figure>
+        </li>
+        <li>
+          <figure>
+            <ant-design-format-painter-outlined />
+            <figcaption>{{ t('Tailored To Your Needs') }}</figcaption>
+          </figure>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <figure>
+            <clarity-ruler-pencil-line />
+            <figcaption>{{ t('Pixel-Perfect Code') }}</figcaption>
+          </figure>
+        </li>
+        <li>
+          <figure>
+            <gg-heart />
+            <figcaption>{{ t('Made With Passion') }}</figcaption>
+          </figure>
+        </li>
+      </ul>
+    </ul>
+  </section>
+</template>
+<style scoped lang="postcss">
+  .skills {
+    ul {
+      @apply flex flex-wrap justify-center gap-10;
+      li figure {
+        @apply bg-[var(--info)] w-88 h-88 text-[var(--background)];
+        & > *:nth-child(1) {
+          @apply w-full h-full;
+        }
+        & > *:nth-child(2) {
+          @apply bg-[var(--foreground-contrast)];
+        }
+      }
     }
   }
 </style>
