bd9335745addbc7e04c4bcfe7b73fa4770338e03
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-12 21:42:37 +0100

    fix: force Introduction section to align to start (=left)

--- app/src/features/LandingPage/core/components/Introduction/Introduction.vue
+++ app/src/features/LandingPage/core/components/Introduction/Introduction.vue
@@ -3,7 +3,7 @@
   const { counter, pause, resume } = useInterval(200, { controls: true })
 </script>
 <template>
-  <section id="introduction" class="!h-screen items-start text-left">
+  <section id="introduction" class="!h-screen !items-start text-left">
     <h1>{{ t('Hi,') }}<br />{{ t("I'm Francois,") }}<br />{{ t('web developer') }}</h1>
     <p class="py-4">Vue.js enthusiast | full-stack dev | Freelancer</p>
   </section>
