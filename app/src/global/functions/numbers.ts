603ad46a40ac16b233932f7f3a94790dd006c97e
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-12 21:10:00 +0100

    feat: implement global func convertHex2Dec

--- app/src/global/functions/numbers.ts
+++ app/src/global/functions/numbers.ts
@@ -1,2 +1,3 @@
 export const range = (end: number, start = 0, step = 1) =>
   Array.from({ length: Math.ceil((end + 1 - start) / step) }, (_, i) => start + i * step)
+export const convertHex2Dec = (hexString: string) => parseInt(hexString, 16)
