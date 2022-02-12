3d7edc59f67884cfc5190f5957e5f3f685409ca1
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-12 21:15:15 +0100

    feat: add 'Location' to 'Contact' section

--- app/src/features/LandingPage/core/components/Contact/Contact.vue
+++ app/src/features/LandingPage/core/components/Contact/Contact.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+  import Location from './Location'
   const { t } = useI18n()
 </script>
 <template>
   <section id="contact">
     <h3> {{ t('Contact') }} </h3>
+    <Location />
   </section>
 </template>
