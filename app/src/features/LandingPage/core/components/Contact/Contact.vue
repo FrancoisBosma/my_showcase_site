42353f78f22a67dee94d74bae2a0d2e28bded6ba
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-13 20:45:06 +0100

    feat: add missing links and layout to Contact

--- app/src/features/LandingPage/core/components/Contact/Contact.vue
+++ app/src/features/LandingPage/core/components/Contact/Contact.vue
@@ -6,9 +6,9 @@
   <section id="contact">
     <h3> {{ t('Contact') }} </h3>
     <h2> {{ t('To contact me') }} </h2>
-    <div class="flex flex-wrap">
+    <div class="flex flex-wrap justify-center gap-32">
       <div class="flex flex-col gap-8">
-        <div>
+        <div class="flex flex-col">
           <p> {{ t('The best way to reach me is') }} </p>
           <p> {{ t('via ...') }} </p>
           <TrapDoor>
@@ -18,10 +18,21 @@
             fbosma.pro@gmail.com
           </TrapDoor>
           <p> {{ t('or ...') }} </p>
+          <a
+            href="https://linkedin.com/in/francois-bosma-1051849b/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="mt-4"
+          >
+            <fontisto-linkedin class="icon-link opacity-100 text-2xl" />
+          </a>
         </div>
-        <div>
+        <div class="flex flex-col">
           <p> {{ t('You can also check out') }} </p>
           <p> {{ t('more of my work on GitHub') }} </p>
+          <a href="https://github.com/FrancoisBosma" target="_blank" rel="noopener noreferrer" class="mt-4">
+            <zmdi-github-alt class="icon-link opacity-100 text-2xl" />
+          </a>
         </div>
       </div>
       <div>
