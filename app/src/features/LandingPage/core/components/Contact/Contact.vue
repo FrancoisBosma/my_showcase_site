3468530d96360b6e2304d7a350607fddb9250fe6
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-13 15:17:53 +0100

    feat: add time zone basic information in Contact

--- app/src/features/LandingPage/core/components/Contact/Contact.vue
+++ app/src/features/LandingPage/core/components/Contact/Contact.vue
@@ -6,6 +6,13 @@
   <section id="contact">
     <h3> {{ t('Contact') }} </h3>
     <h2> {{ t('To contact me') }} </h2>
-    <Location />
+    <div class="flex flex-wrap">
+      <div></div>
+      <div>
+        <p> {{ t('I live in Paris') }} </p>
+        <p> CET+0 // UTC+1 </p>
+        <Location />
+      </div>
+    </div>
   </section>
 </template>
