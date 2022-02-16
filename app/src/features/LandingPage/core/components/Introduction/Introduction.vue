e893b3a606b63dea757a25331a01c6990c062d56
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-16 20:40:11 +0100

    feat: animate 'typed text' like if it was typed in a terminal

--- app/src/features/LandingPage/core/components/Introduction/Introduction.vue
+++ app/src/features/LandingPage/core/components/Introduction/Introduction.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
   const { t } = useI18n()
-  const { counter, pause, resume } = useInterval(200, { controls: true })
+  const tinkeringsTranslation = t('Tinkerings')
+  const bShowCursor = ref(true)
+  const cursorClink = useIntervalFn(() => {
+    bShowCursor.value = !bShowCursor.value
+  }, 750)
+  cursorClink.pause()
+  const subTitle = ref('')
+  const placeholderText = ' '
+  const dynamicSubTitle = ref(placeholderText)
+  const setSubTitle = (text: string) => {
+    subTitle.value = text
+    return dynamicSubTitle.value
+  }
+  const subTitleTypingDirection = ref(1)
+  const textType = useIntervalFn(() => {
+    dynamicSubTitle.value = subTitle.value.substring(0, dynamicSubTitle.value.length + subTitleTypingDirection.value)
+    if (dynamicSubTitle.value.length === subTitle.value.length || dynamicSubTitle.value.length === 0)
+      subTitleTypingDirection.value *= -1
+    if (dynamicSubTitle.value.length === subTitle.value.length) {
+      textType.pause()
+      cursorClink.resume()
+      bShowCursor.value = true
+      useTimeoutFn(() => {
+        textType.resume()
+        cursorClink.pause()
+        bShowCursor.value = true
+      }, 3000)
+    }
+  }, 75)
 </script>
 <template>
   <section id="introduction" class="!h-screen flex flex-col justify-center items-start text-left">
     <h1>{{ t('Hi') }},<br />{{ t("I'm Francois") }},<br />{{ t('web developer') }}</h1>
-    <p class="py-4"> Vue.js | Full-stack | {{ t('Tinkerings') }} </p>
+    <p class="py-4">
+      {{ '$ ' + setSubTitle(`Vue.js | Full-stack | ${tinkeringsTranslation}`)
+      }}<span v-show="bShowCursor" class="bg-[var(--typed-text)]">&#95;</span>
+    </p>
     <a href="#contact">
       <Button> {{ t('Contact') }} </Button>
     </a>
