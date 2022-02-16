cc704c69b28a045848f1c93dee3c51523e4e64ca
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-16 16:56:52 +0100

    feat: finish Introduction's content and layout; style remains to be finished

--- app/src/features/LandingPage/core/components/Introduction/Introduction.vue
+++ app/src/features/LandingPage/core/components/Introduction/Introduction.vue
@@ -3,8 +3,11 @@
   const { counter, pause, resume } = useInterval(200, { controls: true })
 </script>
 <template>
-  <section id="introduction" class="!h-screen !items-start text-left">
+  <section id="introduction" class="!h-screen flex flex-col justify-center items-start text-left">
     <h1>{{ t('Hi') }},<br />{{ t("I'm Francois") }},<br />{{ t('web developer') }}</h1>
-    <p class="py-4">Vue.js enthusiast | full-stack dev | Freelancer</p>
+    <p class="py-4"> Vue.js | Full-stack | {{ t('Tinkerings') }} </p>
+    <a href="#contact">
+      <Button> {{ t('Contact') }} </Button>
+    </a>
   </section>
 </template>
