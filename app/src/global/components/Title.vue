5bbfee5b48ff3f3e35c1c169af2a8a0f0b957476
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-22 19:19:06 +0100

    feat: implement interactive+dynamic title SFC

---
+++ app/src/global/components/Title.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import type { Ref } from 'vue'
+  import type { Stoppable } from '@vueuse/core'
+
+  const props = withDefaults(defineProps<{ text: string }>(), { text: '' })
+  const text = toRef(props, 'text')
+  const letterElements = ref([])
+  const letterAnimations: Ref[] = []
+  const letterAnimationResets: Stoppable[] = []
+  text.value.split('').forEach((_, index: number) => {
+    const newAnimation = ref('')
+    letterAnimations.push(newAnimation)
+    const newAnimationReset = useTimeoutFn(() => (letterAnimations[index].value = ''), 1000, { immediate: false })
+    letterAnimationResets.push(newAnimationReset)
+  })
+  const toto = (index: number) => {
+    letterAnimations[index].value = 'animate-animated animate-wobble text-[var(--emphasis)]'
+    letterAnimationResets[index].start()
+  }
+</script>
+<template>
+  <h1 :aria-label="text">
+    <span
+      v-for="(c, index) in text"
+      :key="index"
+      ref="letterElements"
+      :class="`inline-block ${letterAnimations[index].value}`"
+      @mouseenter="toto(index)"
+    >
+      <!-- w:hover="text-[var(--emphasis)]" -->
+      {{ c === ' ' ? '&nbsp;' : c }}
+    </span>
+  </h1>
+</template>
