33a467d75b652d56db3517e498eb3154f53c3a1d
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-22 20:28:55 +0100

    fix: translation within 'Introduction' SFC is broken

--- app/src/features/LandingPage/core/components/Introduction/Introduction.vue
+++ app/src/features/LandingPage/core/components/Introduction/Introduction.vue
@@ -3,15 +3,16 @@
 
   const { t } = useI18n()
   const subTitle = t('Mainly looking for Vue.js missions / Open to anything')
+  const problematicText = t("I'm Francois")
 
   const sectionElement = ref<HTMLElement | null>(null)
 </script>
 <template>
   <section id="introduction" ref="sectionElement" class="!h-screen flex flex-col justify-center items-start text-left">
     <FractalsTree ref="fractalsElement" :container-element="sectionElement" />
-    <Title :text="`${t('Hi')},`" />
-    <Title :text="`${t('I\'m Francois')},`" />
-    <Title :text="`${t('web developer')}`" />
+    <Title :text="t('Hi') + ','" />
+    <Title :text="t(problematicText) + ','" />
+    <Title :text="t('web developer')" />
     <p class="typed py-4">
       <AnimatedTypedText :text="t(subTitle)" />
     </p>
