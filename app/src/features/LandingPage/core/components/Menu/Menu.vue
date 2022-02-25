f43cdab7cdcf4c17158a201a7efdb9487770794f
FrancoisBosma <francois.bosma@hotmail.fr>
2022-02-25 18:46:23 +0100

    feat: replace placeholder text with proper site logo within Menu

--- app/src/features/LandingPage/core/components/Menu/Menu.vue
+++ app/src/features/LandingPage/core/components/Menu/Menu.vue
@@ -16,16 +16,47 @@
   <div class="responsive-fixed">
     <BurgerButton v-show="!bScreenXl" class="menu-button" :action="fnToggleMenu" :is-active="bMenuActive" />
     <menu>
-      <p class="p-4 text-6xl">FB</p>
-      <nav
-        w:flex="~ col"
-        w:border="[var(--background)] t-1"
-        w:children="border-[var(--background)] border-b-1 leading-20 text-[var(--foreground-for-stronger-background)] text-xl"
-      >
-        <a class="label hover-emphasis" href="#playground" rel="" @click="fnToggleMenu()">{{ t('Playground') }}</a>
-        <a class="label hover-emphasis" href="#skills" rel="" @click="fnToggleMenu()">{{ t('Skills') }}</a>
-        <a class="label hover-emphasis" href="#contact" rel="" @click="fnToggleMenu()">{{ t('Contact') }}</a>
-      </nav>
+      <a class="flex justify-center items-center" href="#introduction" rel="" @click="fnToggleMenu()">
+        <svg
+          id="esAzjGhm7Hf1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 32 32"
+          shape-rendering="geometricPrecision"
+          text-rendering="geometricPrecision"
+          class="h-1/2"
+        >
+          <text
+            dx="0"
+            dy="0"
+            font-family='"esAzjGhm7Hf1:::Spectral"'
+            font-size="1.6"
+            font-weight="400"
+            transform="matrix(29.3319 0 0 26.6539-1.70374 31.9912)"
+            stroke-width="0"
+          >
+            <tspan y="0" font-weight="400" stroke-width="0"><![CDATA[f]]></tspan>
+          </text>
+          <text
+            dx="0"
+            dy="0"
+            font-family='"esAzjGhm7Hf1:::Spectral"'
+            font-size="1.6"
+            font-weight="400"
+            transform="matrix(27.2113 0 0 21.0408 9.75666 31.681953)"
+            stroke-width="0"
+          >
+            <tspan y="0" font-weight="400" stroke-width="0"><![CDATA[b]]></tspan>
+          </text>
+        </svg>
+      </a>
+      <div class="links-nav">
+        <nav>
+          <a class="label" href="#playground" rel="" @click="fnToggleMenu()">{{ t('Playground') }}</a>
+          <a class="label" href="#skills" rel="" @click="fnToggleMenu()">{{ t('Skills') }}</a>
+          <a class="label" href="#contact" rel="" @click="fnToggleMenu()">{{ t('Contact') }}</a>
+        </nav>
+      </div>
       <div w:flex="~ col" class="justify-center">
         <ul class="flex justify-center gap-x-2 py-4 children:children:cursor-default">
           <li>
@@ -49,11 +80,24 @@
     z-index: calc(theme('zIndex.fixed') + 1);
   }
   menu {
-    @apply relative h-full w-full m-0 p-0 z-fixed bg-[var(--background-stronger)] grid grid-rows-[1fr,auto,1fr]
+    @apply relative h-full w-full m-0 p-0 z-fixed bg-[var(--background-stronger)] grid grid-rows-[33%,33%,33%]
       transition-all duration-300 ease-in-out;
     width: v-bind('responsiveMenuWidth');
     transform: translateX(v-bind('menuXOffset'));
     opacity: v-bind('menuOpacity');
+    .links-nav {
+      @apply flex flex-col justify-center border-[var(--background)];
+      nav {
+        @apply flex flex-col justify-center border-t-1 border-[var(--background)];
+        & > a {
+          @apply hover-emphasis border-[var(--background)] border-b-1 leading-20
+            text-[var(--foreground-for-stronger-background)] text-xl;
+        }
+      }
+    }
+    tspan {
+      fill: var(--background);
+    }
   }
   .icon-link {
     @apply hover-emphasis inline-block cursor-pointer select-none opacity-75 text-[var(--cta)]
